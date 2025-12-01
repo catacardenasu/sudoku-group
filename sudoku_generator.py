ba2d96dd410439859a46ed8ac5ceaf157e45edc9
Catalina Cardenas <catacardenasu@gmail.com>
2025-12-01 18:49:33 -0500

    board class

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,4 +1,4 @@
-import math,random
+import math,random, pygame
 
 """
 This was adapted from a GeeksforGeeks article "Program for Sudoku Generator" by Aarti_Rathi and Ankur Trisal
@@ -266,3 +266,74 @@ class Cell:
         pass
 
 
+class Board:
+    def __init__(self, width, height, screen, difficulty)
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+
+    def draw(self):
+        # Draw thin lines for every cell
+        for i in range(10):
+            y = i * 57
+            pygame.draw.line(self.screen, (0, 0, 0), (0, y), (513, y), 1)
+        for j in range(10):
+            x = j * 57
+            pygame.draw.line(self.screen, (0,0,0), (x, 0), (x, 513), 1)
+
+        # Draw bold lines to delineate 3x3 boxes
+        for k in range(0, 10, 3):
+            y = k * 57
+            pygame.draw.line(self.screen, (0,0,0), (0, y), (513, y), 3)
+        for k in range(0, 10, 3):
+            x = k * 57
+            pygame.draw.line(self.screen, (0,0,0), (x, 0), (x, 513), 3)
+
+    def select(self, row, col):
+        pass
+        # Marks the cell at (row, col) in the board as the current selected cell.
+        # Once a cell has been selected, the user can edit its value or sketched value.
+
+    def click(self, x, y):
+        pass
+    # If a tuple of (x,y) coordinates is within the displayed board,
+    # this function returns a tuple of the (row, col) of the cell which was clicked.
+    # Otherwise, this function returns None.
+
+    def clear(self):
+        pass
+    #     Clears the value cell.
+    # Note that the user can only remove the cell values and
+    # sketched values that are filled by themselves.
+
+    def sketch(self, value):
+        pass
+        # Sets the sketched value of the current selected cell equal to the user entered value.
+        # It will be displayed at the top left corner of the cell using the draw() function.
+
+    def place_number(self, value):
+        pass
+    #     Sets the value of the current selected cell equal to the user entered value.
+    # Called when the user presses the Enter key.
+
+    def reset_to_original(self):
+        pass
+        #Resets all cells in the board to their original values
+    #(0 if cleared, otherwise the corresponding digit).
+
+
+    def is_full(self):
+        pass
+        #Returns a Boolean value indicating whether the board is full or not.
+
+    def update_board(self):
+        pass
+        #Updates the underlying 2D board with the values in all cells.
+
+    def find_empty(self):
+        pass
+        #Finds an empty cell and returns its row and col as a tuple (x,y).
+
+    def check_board(self):
+        pass
